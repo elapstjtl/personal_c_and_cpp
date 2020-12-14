f3cc86c658ac4ee1eae5a4f095700bb58cc6aa0e
elapstjtl <elapstjtl1@163.com>
2020-12-14 09:07:20 +0800

    ??

---
+++ Microsoft VS Code/_/resources/app/out/vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.nls",{"vs/base/common/codicons":["Icon for drop down buttons."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/940b5f4bb5fa47866a54529ed759d95d09ee80be/core/vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.nls.js.map
