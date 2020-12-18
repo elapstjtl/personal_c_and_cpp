f1892db22f0951a6f339d23ccc1ab41aae071f74
elapstjtl <elapstjtl1@163.com>
2020-12-18 23:06:30 +0800

    新增代码模板,main   .h

---
+++ cpp/base/object/practice/1.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-18 23:02:50
+ * @LastEditTime: 2020-12-18 23:02:50
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \code\cpp\base\object\practice\1.cpp
+ */
+#include<iostream>
+using namespace std;
+int main(void)
+{
+
+system("pause");
+return 0;
+}
---
+++ cpp/base/object/practice/1.h
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-18 23:05:45
+ * @LastEditTime: 2020-12-18 23:05:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \code\cpp\base\object\practice\1.h
+ */
+#ifndef _MAP_H_
+#define _MAP_H_
+
+#endif
